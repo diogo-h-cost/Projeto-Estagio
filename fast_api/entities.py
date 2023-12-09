2f28c28f2b84af77268db414efcd9103813d29c3
Diogo Lima <diogo.henrique.cdl@gmail.com>
2023-12-09 18:27:34 -0300

    Class de modelos de dados (Tabelas no banco)

---
+++ fast_api/entities.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+
+Base = declarative_base()
+
+class Products(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String(100), nullable=False)
+    value = Column(Float, nullable=False)
+
+class ProductVariants(Base):
+    __tablename__ = 'products_variants'
+
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey('products.id'))
+    model = Column(String(100), nullable=False)
+    color = Column(String(100), nullable=False)
+    size = Column(String(100), nullable=False)
